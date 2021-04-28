bf49b5daa0f79fe8d891af22cf13cc64a21d7e94
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-04-28 09:07:00 -0600

    Minimal repro for prop type error

--- src/Test.vue
+++ src/Test.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    {{ fo }}
-  </div>
+  <span v-if="!onSelect">Not selectable</span>
+  <span v-else>Selectable</span>
 </template>
 
-<script>
-import * as _ from 'lodash'
+<script lang="ts">
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
 
-export default {
-  props: ['foo', 'bar']
-}
+type OnSelectCallback = () => void
+
+export default defineComponent({
+  props: {
+    onSelect: {
+      // error here, but only when the type is marked nullable:
+      type: Function as PropType<OnSelectCallback | null>,
+      default: null
+    }
+  }
+})
 </script>
